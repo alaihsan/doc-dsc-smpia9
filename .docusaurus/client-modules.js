068bb8a852ce5a75cdd0786cddcbc9ae5e9d5348
Muhamad Ikhsan <iksan.qren@gmail.com>
2023-08-19 17:54:56 +0700

    fresh install

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/user/dev/doc-dscsmpia9/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/user/dev/doc-dscsmpia9/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/user/dev/doc-dscsmpia9/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/user/dev/doc-dscsmpia9/src/css/custom.css'),
+];
